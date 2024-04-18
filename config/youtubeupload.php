572cef2972917d66235156170a09b6d022b2c6c8
Pascal Canadas <pascal@mirandais.ovh>
2024-04-18 22:03:08 +0200

    🐛 FIX: Structure du fichier config (Fixed: #6)

--- config/youtubeupload.php
+++ config/youtubeupload.php
@@ -2,5 +2,10 @@
 
 // config for Leknoppix/YoutubeUpload
 return [
-
+    'youtube' => [
+        'developer_key' => env('YOUTUBE_API_KEY'), // Clé publique développeur
+        'clientID' => env('YOUTUBE_CLIENT_ID'),
+        'clientSecret' => env('YOUTUBE_CLIENT_SECRET'),
+        'redirect' => env('YOUTUBE_REDIRECT'),
+    ]
 ];
