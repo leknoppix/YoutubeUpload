f76dcdf532a7db9caa2b2ea73615d6cba9b4ce4a
Pascal Canadas <pascal@mirandais.ovh>
2024-04-19 17:43:25 +0200

    fix: 🐛 Ajout des principales routes (sans controller)
    Préparation des routes basiques
    
    ✅ Closes: #8

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,15 @@ $prefix = config('youtubeupload.prefix');
 Route::group(['namespace' => 'Admin', 'prefix' => $prefix], function () {
     //Url de connexion à youtube
     Route::get('/youtubeupload/index', function () {
-        echo 'OK';
+        echo 'Route qui listera les chaînes ayant un token (actif ou pas)';
     });
-    //Url de retour
-
-    //Enregistrement du Token
+    Route::get('/youtubeupload/add', function () {
+        echo 'Route qui permettra de créer une chaine';
+    });
+    Route::get('/youtubeupload/edit/{id}', function () {
+        echo 'Route qui permettra de modifier une chaine';
+    });
+    Route::get('/youtubeupload/delete/{id}', function () {
+        echo 'Route qui permettra de supprimer une chaine';
+    });
 });
