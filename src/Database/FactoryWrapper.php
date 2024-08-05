06e5238e00f7603322495bbccb232e84fbbf07b9
Pascal Canadas <pascal@mirandais.ovh>
2024-08-05 21:35:05 +0200

    feat: 🎸 Listing des videos et de sa pagination
    ✅ Closes: #24

--- src/Database/FactoryWrapper.php
+++ src/Database/FactoryWrapper.php
@@ -11,16 +11,40 @@ class FactoryWrapper
      */
     protected static $namespace;
 
-    public static function useNamespace($namespace): void
+    public static function useNamespace(string $namespace): void
     {
         self::$namespace = $namespace;
-        Factory::guessFactoryNamesUsing(function (string $modelName) use ($namespace) {
-            return $namespace.'\\'.$modelName.'Factory';
-        });
+        Factory::guessFactoryNamesUsing(
+            /**
+             * @param  class-string<Illuminate\Database\Eloquent\Model>  $modelName
+             * @return class-string<Illuminate\Database\Eloquent\Factories\Factory>
+             */
+            function (string $modelName): string {
+                $fqnFactoryName = self::getFullyQualifiedFactoryName($modelName);
+
+                if (! class_exists($fqnFactoryName)) {
+                    throw new \RuntimeException('The factory '.$fqnFactoryName.' does not exist');
+                }
+
+                return $fqnFactoryName;
+            }
+        );
     }
 
     public static function getNamespace(): ?string
     {
         return self::$namespace;
+    }
+
+    private static function getFullyQualifiedFactoryName(string $modelName): string
+    {
+        $factoryName = class_basename($modelName).'Factory';
+
+        if (self::$namespace) {
+            return self::$namespace.'\\'.$factoryName;
+        }
+
+        // Default to just the model name with 'Factory'
+        return $modelName.'Factory';
     }
 }
